3b3bc42a66a94494ce6a7f1d94159a8861edcce0
arnabw <arnabsahawrk@gmail.com>
2024-05-19 11:01:15 +0600

    parallax section added

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import HomeBanner from "../Banner/HomeBanner";
 import OrderOnlineSection from "./OrderOnlineSection";
+import ParallaxSection from "./ParallaxSection";
 
 const Home = () => {
   return (
     <>
       <HomeBanner />
       <OrderOnlineSection />
+      <ParallaxSection />
     </>
   );
 };
---
+++ client/src/components/Home/ParallaxSection.jsx
@@ -0,0 +1,30 @@
+import Container from "../common/Container/Container";
+import chefService from "../../assets/home/chef-service.jpg";
+import { Parallax } from "react-parallax";
+
+const ParallaxSection = () => {
+  return (
+    <Parallax
+      blur={5}
+      bgImage={chefService}
+      strength={200}
+      bgImageAlt="Chef Service"
+      className="bg-cover bg-center bg-no-repeat bg-fixed"
+    >
+      <Container className="md:py-[119px] py-[60px]">
+        <div className="bg-white py-[10px] md:py-[96px] px-[4px] md:px-[112px] text-center text-[#151515] space-y-2">
+          <h1 className="text-xl md:text-[45px] font-bold">Bistro Boss</h1>
+          <p className="text-xs md:text-base">
+            Continually evisculate impactful intellectual capital with
+            interdependent solutions. Conveniently recaptiualize sustainable
+            meta-services rather than technically sound channels. Monotonectally
+            unleash alternative e-business vis-a-vis market-driven niche
+            markets. Assertively foster competitive value after.
+          </p>
+        </div>
+      </Container>
+    </Parallax>
+  );
+};
+
+export default ParallaxSection;
